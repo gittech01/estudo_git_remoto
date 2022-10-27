127deb84729cb64cc5b78601bef7ba2fe2951a88
Edson Consegue <edsonconsegue990@gmail.com>
2022-10-27 13:04:48 -0300

    tabela

---
+++ Projeto/main.js
@@ -0,0 +1,18 @@
+$(document).ready(async function () {  //quando o ducumento estiver pronto, execute a função que estivar dentro do parenteses
+
+const resposta = await axios.get("https://api.publicapis.org/entries", );
+console.log(resposta.data);
+
+$("#corpo").append("<tr>"+
+'<th scope="row">'+1+'</th>'+
+'<td>Facebook</td>'+
+'<td>2.0</td>'+
+'<td>gghhh</td>'+
+'<td>ffggg</td>'+
+'td>668h</td>'+
+'<td>nmmmhh</td>'+
+'</tr>'
+);
+
+}); 
+
